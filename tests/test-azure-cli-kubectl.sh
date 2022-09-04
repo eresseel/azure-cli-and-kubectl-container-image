8c8cb1968f50334b42aff51d36ab481944b23995
lberes <laszlo.beres@nng.com>
2022-09-04 17:32:14 +0200

    [OS-3] Add test cases

---
+++ tests/test-azure-cli-kubectl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\e[1;33m Run test...\e[0m"
+
+if [ -f "/usr/bin/az" ]; then
+  echo -e "\e[1;32m The 'az' command is exists.\e[0m"
+  az --version
+else
+  echo -e "\e[1;31m Error: The 'az' command not found. Test is failed.\e[0m"
+  exit 1
+fi
+
+if [ -f "/usr/bin/kubectl" ]; then
+  echo -e "\e[1;32m The 'kubectl' command is exists.\e[0m"
+  kubectl version
+else
+  echo -e "\e[1;31m Error: The 'kubectl' command not found. Test is failed.\e[0m"
+  exit 1
+fi
